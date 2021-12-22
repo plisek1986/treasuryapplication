ce49169ea4b6ff62c37e1543f37a853eb1731255
Pawel <pawel.lisek.fp@gmail.com>
2021-12-22 19:40:55 +0100

    migrations between Users and Accounts fixed

--- BankingAdministration/Accounts/models.py
+++ BankingAdministration/Accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Users.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    """ Class defines attributes for each account object"""
+
+    iban_number = models.CharField(max_length=64, unique=True, blank=False, null=False)
+    swift_code = models.CharField(max_length=11, blank=False)
+    bank = models.ForeignKey('Bank', on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    user = models.ManyToManyField('Users.User')
+
+
+class Bank(models.Model):
+    pass
+
+
+class Company(models.Model):
+    pass
+
--- BankingAdministration/Users/models.py
+++ BankingAdministration/Users/models.py
@@ -5,18 +5,35 @@ from django.contrib.auth.models import AbstractUser
 # we create User from AbstractUser to allow us for changes to a predefined model of Django User, in case those are
 # needed in the future
 class User(AbstractUser):
-    account = models.ManyToManyField('Account')
     email = models.EmailField(unique=True, max_length=255)
     is_payment_creator = models.BooleanField(default=False)
     is_payment_approver = models.BooleanField(default=False)
     can_delete_payment = models.BooleanField(default=False)
-    access = models.ManyToManyField('Access')
     username = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        app_label = 'Users'
 
-class Account(models.Model):
-    pass
+    def __str__(self):
+        return self
 
 
-class Access(models.Model):
-    pass
+# class Account(models.Model):
+#     """ Class defines attributes for each account object"""
+#
+#     iban_number = models.CharField(max_length=64, unique=True, blank=False, null=False)
+#     swift_code = models.CharField(max_length=11, blank=False)
+#     bank = models.ForeignKey('Bank', on_delete=models.CASCADE)
+#     company = models.ForeignKey('Company', on_delete=models.CASCADE)
+#     user = models.ManyToManyField(User)
+#
+#     def __str__(self):
+#         return self
+#
+#
+# class Bank(models.Model):
+#     pass
+#
+#
+# class Company(models.Model):
+#     pass
