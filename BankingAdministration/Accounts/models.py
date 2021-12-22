3d82d109c85de8721f58ea3309546b1948b6ddca
Pawel <pawel.lisek.fp@gmail.com>
2021-12-22 21:56:06 +0100

    Accounts application with majority of views

--- BankingAdministration/Accounts/models.py
+++ BankingAdministration/Accounts/models.py
@@ -2,6 +2,40 @@ from django.db import models
 from Users.models import User
 
 
+COUNTRY_CHOICE = [
+    ('Austria', 'AT'),  # Austria
+    ('Belgium', 'BE'),  # Belgium
+    ('Bulgaria', 'BG'),  # Bulgaria
+    ('Switzerland', 'CH'),  # Switzerland
+    ('Czech Republic', 'CZ'),  # Czech Republic
+    ('Germany', 'DE'),  # Germany
+    ('Denmark', 'DK'),  # Denmark
+    ('Estonia', 'EE'),  # Estonia
+    ('Spain', 'ES'),  # Spain
+    ('Finland', 'FI'),  # Finland
+    ('France',  'FR'),  # France
+    ('United Kingdom', 'GB'),  # United Kingdom
+    ('Greece', 'GR'),  # Greece
+    ('Croatia','HR'),  # Croatia
+    ('Hungary', 'HU'),  # Hungary
+    ('Ireland', 'IE'),  # Ireland
+    ('Iceland', 'IS'),  # Iceland
+    ('Italy', 'IT'),  # Italy
+    ('Kazakhstan', 'KZ'),  # Kazakhstan
+    ('Lithuania', 'LT'),  # Lithuania
+    ('Latvia', 'LV'),  # Latvia
+    ('Netherlands', 'NL'),  # Netherlands
+    ('Norway', 'NO'),  # Norway
+    ('Poland', 'PL'),  # Poland
+    ('Portugal', 'PT'),  # Portugal
+    ('Romania', 'RO'),  # Romania
+    ('Sweden', 'SE'),  # Sweden
+    ('Slovenia', 'SI'),  # Slovenia
+    ('Slovakia', 'SK'),  # Slovakia
+    ('Turkey', 'TR'),  # Turkey
+]
+
+
 class Account(models.Model):
     """ Class defines attributes for each account object"""
 
@@ -9,7 +43,7 @@ class Account(models.Model):
     swift_code = models.CharField(max_length=11, blank=False)
     bank = models.ForeignKey('Bank', on_delete=models.CASCADE)
     company = models.ForeignKey('Company', on_delete=models.CASCADE)
-    user = models.ManyToManyField('Users.User')
+    user = models.ManyToManyField(User)
 
 
 class Bank(models.Model):
--- BankingAdministration/Users/models.py
+++ BankingAdministration/Users/models.py
@@ -15,25 +15,4 @@ class User(AbstractUser):
         app_label = 'Users'
 
     def __str__(self):
-        return self
-
-
-# class Account(models.Model):
-#     """ Class defines attributes for each account object"""
-#
-#     iban_number = models.CharField(max_length=64, unique=True, blank=False, null=False)
-#     swift_code = models.CharField(max_length=11, blank=False)
-#     bank = models.ForeignKey('Bank', on_delete=models.CASCADE)
-#     company = models.ForeignKey('Company', on_delete=models.CASCADE)
-#     user = models.ManyToManyField(User)
-#
-#     def __str__(self):
-#         return self
-#
-#
-# class Bank(models.Model):
-#     pass
-#
-#
-# class Company(models.Model):
-#     pass
+        return self.username
