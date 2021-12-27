9e3fada8afadddf0174acc9eba81ac0c7334407f
Pawel <pawel.lisek.fp@gmail.com>
2021-12-27 11:57:20 +0100

    remaining views for Banks

--- BankingAdministration/Banks/views.py
+++ BankingAdministration/Banks/views.py
@@ -1,15 +1,15 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views import View
-from django.views.generic import ListView, UpdateView
-from django.views.generic.edit import UpdateView
-from .models import Bank
-from django.http import HttpRequest, HttpResponse, Http404
-from .forms import BankCreateForm, BankUpdateForm
 from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.views.generic.detail import DetailView
+from .models import Bank
 
 
-class BankListView(ListView):
+class BankListView(LoginRequiredMixin, ListView):
     """View for enlisting all banks available in the database."""
 
     model = Bank
@@ -20,25 +20,14 @@ class BankListView(ListView):
         return context
 
 
-class BankCreateView(View):
+class BankCreateView(CreateView):
     """
     View for adding a new bank where administrator can define names for new banks.
     Additional validation for existence of a new bank in the database takes place.
     """
 
-    # def get(self, request, *args, **kwargs):
-    #     form = BankCreateForm()
-    #     return render(request, 'bank_create.html', {'form': form})
-    #
-    # def post(self, request, *args, **kwargs):
-    #     form = BankCreateForm(request.POST)
-    #     if form.is_valid():
-    #         name = form.cleaned_data['name']
-    #         if Bank.objects.filter(name=name):
-    #             message = 'This bank already exists in database.'
-    #             return render(request, 'bank_create.html', {'form': form, 'message': message})
-    #         Bank.objects.create(name=name)
-    #         return redirect('/Banks/bank_list')
+    model = Bank
+    fields = ['name']
 
 
 class BankUpdateView(UpdateView):
@@ -49,25 +38,14 @@ class BankUpdateView(UpdateView):
     template_name_suffix = '_update_form'
 
 
-def bank_delete(request, bank_id, *args, **kwargs):
-    """
-    Function for deleting bank from the database. All accounts associated with this bank are also deleted from the database.
-    :param request: used by Django to pass state through the system
-    :param bank_id: bank to be deleted from the database
-    :return: administrator is redirected to the list of banks
-    """
-
-    bank = Bank.objects.get(pk=bank_id)
-    if request.method == 'POST':
-        bank.delete()
-        return redirect('/Banks/bank_list')
-    return render(request, 'bank_delete.html', {'bank': bank})
+class BankDeleteView(DeleteView):
+    model = Bank
+    success_url = reverse_lazy('Banks:bank-list')
 
 
-class BankViewView(View):
+class BankDetailView(DetailView):
     """View for displaying all details of a bank, incl. it's accounts."""
 
-    def get(self, request, bank_id):
-        bank = Bank.objects.get(pk=bank_id)
-        accounts = bank.account_set.all()
-        return render(request, 'bank_view.html', {'bank': bank, 'accounts': accounts})
+    model = Bank
+
+
