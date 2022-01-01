b992a77112f24aa1dad860539a8248ca3b8b19d9
Pawel <pawel.lisek.fp@gmail.com>
2022-01-01 14:12:14 +0100

    CompanyDetailView complete

--- BankingAdministration/Companies/urls.py
+++ BankingAdministration/Companies/urls.py
@@ -6,7 +6,7 @@ app_name = 'Companies'
 urlpatterns = [
     path('company_list/', CompanyListView.as_view(), name='company-list'),
     path('company_create/', CompanyCreateView.as_view(), name='company-create'),
-    path('company_create/<int:pk>/', CompanyDetailView.as_view(), name='company-detail'),
+    path('company_detail/<int:pk>/', CompanyDetailView.as_view(), name='company-detail'),
     path('company_edit/<int:pk>/', CompanyUpdateView.as_view(), name='company-edit'),
     path('company_delete/<int:pk>/', CompanyDeleteView.as_view(), name='company-delete'),
 ]
